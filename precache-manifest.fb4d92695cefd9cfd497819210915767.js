1be93f213641457b1e73967fe4fc12d23a2a75f3
unknown <C:\Users\Serp\AppData\Roaming\The Bat!>
2019-11-19 17:29:28 +0500

    load prodject

---
+++ precache-manifest.fb4d92695cefd9cfd497819210915767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0d9be3a4b4f499a48b5598df17b7cc",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "a36dafcd90188c4023ec",
+    "url": "/demo/static/css/main.de38182d.chunk.css"
+  },
+  {
+    "revision": "ea3e39b67f93ea612ade",
+    "url": "/demo/static/js/2.777c322c.chunk.js"
+  },
+  {
+    "revision": "bc62bd0dca98d4f2a649",
+    "url": "/demo/static/js/3.750e0f53.chunk.js"
+  },
+  {
+    "revision": "a36dafcd90188c4023ec",
+    "url": "/demo/static/js/main.0a240f63.chunk.js"
+  },
+  {
+    "revision": "39ed6e5e121ca2e03bd2",
+    "url": "/demo/static/js/runtime~main.031c6380.js"
+  },
+  {
+    "revision": "62fbc2af49cb90fa80723f7a145d9f6e",
+    "url": "/demo/static/media/default_foto_user.62fbc2af.jpg"
+  }
+]);
